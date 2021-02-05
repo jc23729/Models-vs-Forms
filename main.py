7b5f53a4171af060b22cf4232c686699de086122
Juan Cruz <jc23729@gmail.com>
2021-02-05 11:38:20 +0000

    notes

---
+++ main.py
@@ -0,0 +1,8 @@
+# SQLAlchemy provides model: class for logical object
+# WTForm provides form class
+# A single model may have different forms
+# Not all fields on add form might appear on edit form
+# Different validation might apply on add/edit
+# Different kinds of users (public v admin) have different fields
+# You’ll often take the result of a form and create/edit an SQLAlchemy object.
+
